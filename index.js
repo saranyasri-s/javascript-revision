ddec207777d29b53af092d8ab517a240eb854fb7
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-10-04 05:37:01 +0530

    learnt function alline and execution

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 let a = "saranyasri";
 console.log(a);
+if (1 == "1") {
+  console.log("==");
+}
+if (1 === 1) {
+  console.log("===");
+} else {
+  console.log(false);
+}
+// checking execution of function in diff cases
+function check() {
+  console.log("function executed");
+  return true;
+}
+let fuc = check();
+console.log(fuc);
+
+// doing the given task in udemy
+function combine(a, b, c) {
+  const result = (a * b) / c;
+  return result;
+}
+let taskFunction = combine(1, 2, 3);
+console.log(taskFunction);
