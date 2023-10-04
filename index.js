a9084e7b3f88aaf1eb870c14b2cbd9278dd1c69d
saranyasri-s <dentist.apassionateprogrammer@gmail.com>
2023-10-04 07:33:46 +0530

    added task2 given in udemy course

--- index.js
+++ index.js
@@ -29,3 +29,20 @@ const arr1 = [1, 2, 3];
 console.log(arr1[1]);
 arr1.push(8);
 console.log(arr1[3]);
+// task2
+
+
+// Exercise: Array Methods
+// Your task is to add the missing logic to a transformToObjects() function that should transform a list of numbers into a list of JavaScript objects.
+
+// In the newly returned array, every object must have a val key and the input array's number as a value.
+
+// For example, for the provided input [1, 2, 3] the transformToObjects([1, 2, 3]) function should return [{val: 1}, {val: 2}, {val: 3}].
+
+function transformToObjects(numberArray) {
+  // Todo: Add your logic
+  // should return an array of objects
+  let ans = numberArray.map((number) => ({ val: number }));
+  return ans;
+}
+console.log(transformToObjects([1, 2, 3, 4]));
